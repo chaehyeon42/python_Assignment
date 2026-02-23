fb34d888e8cb3449bf3ffba895203817290dbb0b
chaehyeon42 <qpqp8899@naver.com>
2026-02-23 12:34:12 +0900

    파이썬기본 연습문제

---
+++ 연습문제_파이썬기본/__pycache__/main_program.py
@@ -0,0 +1,7 @@
+import math_operations as mo
+
+# 출력 결과
+print(f"원의 넓이: {mo.get_circle_area(5)}")
+print(f"직사각형 넓이: {mo.get_rectangle_area(10, 5)}")
+print(f"팩토리얼 5! = {mo.get_factorial(5)}")
+print(f"최대공약수(48, 18) = {mo.get_gcd(48, 18)}")
